036709259216be93e9cc36a74303196bbdb0f0c5
beshup <ishanghimire9@yahoo.ca>
2020-05-15 17:45:38 -0700

    testing post route to db

--- app.js
+++ app.js
@@ -20,9 +20,27 @@ app.use('/', express.static('client'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.get('/getRegionData', function(req, res){
-    var data;
-    res.send(data);
+app.post('/storePastCovidTweets', function(req, res){
+    var arr = 
+    [ 
+        {content: "tweet text 1", sentiment: 7, createdAt: new Date()}, 
+        {content: "tweet text 2", sentiment: 9, createdAt: new Date()},
+        {content: "tweet text 3", sentiment: 3, createdAt: new Date()},
+        {content: "tweet text 4", sentiment: 2, createdAt: new Date()},
+        {content: "tweet text 5", sentiment: 18, createdAt: new Date()},
+        {content: "tweet text 6", sentiment: 11, createdAt: new Date()}
+    ];
+
+    PastCovidTweet.insertMany(arr, (error, docs) => {
+        if(error){
+            console.log("Couldn't post past covid tweets to db");
+        }
+    });
+    
+});
+
+app.post('/newCovidTweets', function(req, res){
+    
 });
 
 // Listen on specified port
